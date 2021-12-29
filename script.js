ec3299fe9a33492eb179301422458e9e8d635a21
gulmirakhonmusaeva <gulmirakhonmusaeva@gmail.com>
2021-12-30 01:57:52 +0500

    Add all files

---
+++ script.js
@@ -0,0 +1,53 @@
+let basPriceEl = document.querySelector('.basic-price')
+let proPriceEl = document.querySelector('.professional-price')
+let mastPriceEl = document.querySelector('.master-price')
+let toggleEl = document.querySelector('.toggle')
+let toggleBtnEl = document.querySelector('.toggle-btn')
+let annualEl = document.querySelector('.annual')
+let monthEl = document.querySelector('.month')
+let valueEl = document.querySelector('.value')
+
+
+function annual() {
+    monthEl.classList.remove('clicked')
+    annualEl.classList.add('clicked')
+    basPriceEl.textContent= '199.99'
+    proPriceEl.innerHTML= '249.99'
+    mastPriceEl.innerHTML= '399.99'
+}
+
+function month() {
+      annualEl.classList.remove('clicked')
+      monthEl.classList.add('clicked')
+      basPriceEl.innerHTML= '19.99'
+      proPriceEl.innerHTML= '24.99'
+      mastPriceEl.innerHTML= '39.99'
+}
+
+annualEl.addEventListener('click', function(){
+    annual()
+})
+
+monthEl.addEventListener('click', function(){
+    month()
+})
+
+// toggleBtnEl.addEventListener('click', function(){
+//     // let isMonth = toggleBtnEl.classList.contains('month')
+//     let isAnnual = toggleBtnEl.classList.contains('annual')
+//         // annualEl.classList.add('clicked')
+//         // basPriceEl.textContent= '199.99'
+//         // proPriceEl.innerHTML= '249.99'
+//         // mastPriceEl.innerHTML= '399.99'
+//     if (isAnnual) {
+//         annual()
+//     } else { 
+//         // basPriceEl.innerHTML= '19.99'
+//         // proPriceEl.innerHTML= '24.99'
+//         // mastPriceEl.innerHTML= '39.99'
+//         month()
+//     }
+// })
+
+
+
