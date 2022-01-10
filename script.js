f8db9f63bc129e4021724d710274e1f0dd189803
gulmirakhonmusaeva <gulmirakhonmusaeva@gmail.com>
2022-01-10 22:25:25 +0500

    Add all changes

--- script.js
+++ script.js
@@ -1,36 +1,18 @@
-let basPriceEl = document.querySelector('.basic-price')
-let proPriceEl = document.querySelector('.professional-price')
-let mastPriceEl = document.querySelector('.master-price')
-let toggleEl = document.querySelector('.toggle')
-let toggleBtnEl = document.querySelector('.toggle-btn')
-let annualEl = document.querySelector('.annual')
-let monthEl = document.querySelector('.month')
-let valueEl = document.querySelector('.value')
 
+const elsPaymentPeriodInput = document.querySelectorAll('[name="payment-period"]')
+const prices = document.querySelectorAll('.price')
 
-function annual() {
-    monthEl.classList.remove('clicked')
-    annualEl.classList.add('clicked')
-    basPriceEl.textContent= '199.99'
-    proPriceEl.innerHTML= '249.99'
-    mastPriceEl.innerHTML= '399.99'
-}
-
-function month() {
-      annualEl.classList.remove('clicked')
-      monthEl.classList.add('clicked')
-      basPriceEl.innerHTML= '19.99'
-      proPriceEl.innerHTML= '24.99'
-      mastPriceEl.innerHTML= '39.99'
-}
-
-annualEl.addEventListener('click', function(){
-    annual()
-})
-
-monthEl.addEventListener('click', function(){
-    month()
-})
-
-
-
+elsPaymentPeriodInput.forEach(elPaymentPeriodInput => {
+    elPaymentPeriodInput.addEventListener('change', function() {
+        const period = elPaymentPeriodInput.value
+        if(period === 'annual'){
+            prices.forEach((price) =>{
+                price.textContent = +price.getAttribute('data-annually')
+            })
+        } else {
+            prices.forEach((price) =>{
+                price.textContent = +price.getAttribute('data-monthly')
+            })
+        }
+    })
+});
